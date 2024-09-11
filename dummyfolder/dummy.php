57a2aad2f89cfd9ab45c335c66f5080d57cafbe1
zidaslam <aslamzid111@gmail.com>
2024-09-11 15:13:01 +0530

    This is tag file

---
+++ dummyfolder/dummy.php
@@ -0,0 +1,13 @@
+@extends('frontend.frontend-page-master')
+@section('content')
+    @include('frontend.partials.pages-portion.dynamic-page-builder-part', ['page_post' => $page_details])
+@endsection
+@section('left_side_content')
+    {!! \App\PageBuilder\PageBuilderSetup::render_frontend_pagebuilder_content_for_dynamic_page('dynamic_page_left_sidebar', $page_details->id) !!}
+@endsection
+@section('right_side_content')
+    {!! \App\PageBuilder\PageBuilderSetup::render_frontend_pagebuilder_content_for_dynamic_page('dynamic_page_right_sidebar', $page_details->id) !!}
+@endsection
+
+
+<h2>This is dummy folder</h2>
